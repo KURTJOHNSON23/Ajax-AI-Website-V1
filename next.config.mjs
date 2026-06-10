a530052193b4fc2a2102f320833e72533ade0db5
Kurt Johnson <kurtjohnson23@gmail.com>
2026-06-10 16:42:17 +0100

    Revert no-cache headers to fix 503

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-cache, no-store, must-revalidate",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
